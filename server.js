0bce4b9408fe02676c90ed0990e74a193ba6bcf8
jsong73 <jessicajinsong@gmail.com>
2022-10-20 23:48:38 -0400

    making the server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express();
+
+const sequelize = require("./config/connection")
+
+const PORT = process.env.PORT || 3000;
+
+
+sequelize.sync().then(() => {
+    app.listen(PORT, (err) =>
+    console.log(`listening to port ${PORT}`));
+});
