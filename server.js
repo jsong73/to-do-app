9a75f32308b9d5c1b4de4a41f5ad0f8d3124eb9a
jsong73 <jessicajinsong@gmail.com>
2022-10-21 15:32:38 -0400

    pushing again just in case to work on later

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+//to start the server
 const express = require("express")
+//calling the express variable to use express library
+//to start the server
 const app = express();
 const sequelize = require("./config/connection")
 const tasks = require("./models/Tasks")
 
 //connect to the available port if not specified use port 3000
+//to start the server
 const PORT = process.env.PORT || 3000;
 
 //to connect to port using express
+//first parameter is the port number and second parameter is the callback function
+//to start the server
 sequelize.sync().then(() => {
     app.listen(PORT, (err) =>
     console.log(`listening to port ${PORT}`));
